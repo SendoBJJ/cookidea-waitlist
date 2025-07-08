e7ff3831f2fa696c9bd932a39931ec63df5181c7
y.karouite <y.karouite@ch.local>
2025-07-08 16:30:06 +0200

    Enlevez les deco verts bug

--- app/page.tsx
+++ app/page.tsx
@@ -35,9 +35,7 @@ export default function WaitlistPage() {
         <section className="flex flex-col items-center text-center py-24 md:py-32 pt-40">
           <div className="relative">
             <h1 className="text-5xl md:text-6xl font-bold leading-tight z-10 relative">
-              <Image src="/group-1.svg" alt="Décoration" width={96} height={96} className="absolute -top-12 -left-25 w-24"  />
               Cuisinez mieux, ensemble
-              <Image src="/group-2.svg" alt="Décoration" width={96} height={96} className="absolute -bottom-12 -right-25 w-24"  />
             </h1>
           </div>
           <p className="max-w-xl mt-6 text-lg text-gray-300">
